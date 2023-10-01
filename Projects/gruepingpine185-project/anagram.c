53c75a62b9e1d619dc5f5fceb8c4505a80121160
gruelingpine185 <66073333+gruelingpine185@users.noreply.github.com>
2023-10-01 15:05:12 -0400

    removed comment

--- Projects/gruepingpine185-project/anagram.c
+++ Projects/gruepingpine185-project/anagram.c
@@ -1,32 +1,3 @@
-// const int CHAR = 256;
-// bool areanagram(string &s1, string &s2){
-//     if(s1.length()!= s2.length()){
-//         return false;
-//     }
-//     else{
-//         int count[CHAR] = {0};
-//         for(int i=0;i<s1.length();i++){
-//             count[s1[i]]++;
-//             count[s2[i]]--;
-//         }
-//         for(int i=0;i<s1.length();i++){
-//             if(count[i]!=0)
-//                 return false;
-//         }return true;
-//     }
-// }
-//  int main(){
-//     string s1 = "listen";
-//     string s2 = "silent";
-    
-//     if(areanagram(s1,s2)==1)
-//         cout<<"are anagram";
-//     else
-//         cout<<"not anagram";
-        
-//     return 0;
-//  }
-
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
