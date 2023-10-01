d1c279fc114d30a25f87f4ef96d9c5b0bda763dd
SAGARSINGH-1 <sagar01.ko@gmail.com>
2023-10-01 23:57:48 +0530

    Adding Own profile to the home or Profile.jsx file

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,35 +1,32 @@
-import React from 'react'
-import User from '../Contributer/User.js'
-import './Profile.css'
+import React from 'react';
+import User from '../Contributer/User.js';
+import './Profile.css';
+
 const Profile = () => {
   return (
-
-<>
-<div id='profileHeading'> Profiles of Contributers</div>
-{
-  User.map((item)=>{
-    return(
-      <>
-       <div className="container">
-      <div className="box">
-        <img src={`https://avatars.githubusercontent.com/${item.github_username}`}
- alt="alok" height={200} />
-        <h3 className="id"> {item.name}</h3>
-        <p className="description"> {item.about} </p>
-        <button> <a href={item.linkdenProfile}>Linkden</a></button> 
-        <button> <a href = {`https://github.com/${item.github_username}`}>  Github</a></button>
-       
-
-      </div>
+    <div>
+      <div id='profileHeading'>Profiles of Contributors</div>
+      {User.map((item) => (
+        <div className="container" key={item.github_username}>
+          <div className="box">
+            <img
+              src={`https://avatars.githubusercontent.com/${item.github_username}`}
+              alt="alok"
+              height={200}
+            />
+            <h3 className="id"> {item.name}</h3>
+            <p className="description"> {item.about} </p>
+            <button>
+              <a href={item.linkedinProfile}>LinkedIn</a>
+            </button>
+            <button>
+              <a href={`https://github.com/${item.github_username}`}>GitHub</a>
+            </button>
+          </div>
+        </div>
+      ))}
     </div>
-      </>
-    )
-  })
-}
+  );
+};
 
-
-</>
-  )
-}
-
-export default Profile
+export default Profile;
