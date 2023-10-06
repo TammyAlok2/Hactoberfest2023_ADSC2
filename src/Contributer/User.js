ab5e9738452775979f74989380deda862d482a61
Vinay Kokate <vinaynkokate@gmail.com>
2023-10-06 14:41:34 +0530

    Add feature-name

--- src/Contributer/User.js
+++ src/Contributer/User.js
@@ -1,45 +1,54 @@
 // this is contributers data
 
-
-const user =[
-    
-    {
-        'name':'Alok Tamrakar',
-        'github_username':'tammyalok2',
-        'about':'Hello I am alok tamrakar , mern stack developer , open source contributer.',
-        'linkedinProfile':' https://in.linkedin.com/in/alok-tamrakar-89a71a199',
-    },
-    {
-        'name':'Sagar Singh',
-        'github_username':'SAGARSINGH-1',
-        'about':'Hello I am Sagar Singh , Frontend web developer , open source contributer.',
-        'linkedinProfile':'https://www.linkedin.com/in/sagar-singh-97a600221/',
-    },
-    {
-        'name':'Likeur off',
-        'github_username':'Likeur',
-        'about':'Hello I am Likeur , Frontend web developer , open source contributer.',
-        'linkedinProfile':'https://www.linkedin.com/in/likeur-off-47984327a/',
-    },
-    {
-        'name':'Mohasin Haque',
-        'github_username':'Mohasin-Haque',
-        'about':'Hello I am Mohasin , Frontend web developer , open source contributer.',
-        'linkedinProfile':'https://www.linkedin.com/in/mohasin-haque-759786200/',
-    },
-=======
-        'name':'Prathamesh',
-        'github_username': 'Prathamesh1211',
-        'about':'Hello, Prathamesh this side , I am a MERN stack developer and open source contributor.',
-        'linkdenProfile':'https://www.linkedin.com/in/prathamesh-nikumbh-a22485246/',
-    },
-    {
-        'name':'Gokul',
-        'github_username':'gokulz',
-        'about':'Hello I am gokul , java developer , open source contributer.',
-        'linkedinProfile':'https://www.linkedin.com/in/gokul-s-7610ab1a9/',
-    },
-  
-]
+const user = [
+  {
+    name: "Alok Tamrakar",
+    github_username: "tammyalok2",
+    about:
+      "Hello I am alok tamrakar , mern stack developer , open source contributer.",
+    linkedinProfile: " https://in.linkedin.com/in/alok-tamrakar-89a71a199",
+  },
+  {
+    name: "Sagar Singh",
+    github_username: "SAGARSINGH-1",
+    about:
+      "Hello I am Sagar Singh , Frontend web developer , open source contributer.",
+    linkedinProfile: "https://www.linkedin.com/in/sagar-singh-97a600221/",
+  },
+  {
+    name: "Likeur off",
+    github_username: "Likeur",
+    about:
+      "Hello I am Likeur , Frontend web developer , open source contributer.",
+    linkedinProfile: "https://www.linkedin.com/in/likeur-off-47984327a/",
+  },
+  {
+    name: "Vinay Kokate",
+    github_username: "VinayKokate22",
+    about:
+      "Im a MERN stack web developer.I design and code beautifully simple things, and I love what I do.Welcome to this space. Im thrilled to have you here.",
+    linkdenProfile: "https://www.linkedin.com/in/vinay-kokate-318250215/",
+  },
+  {
+    name: "Mohasin Haque",
+    github_username: "Mohasin-Haque",
+    about:
+      "Hello I am Mohasin , Frontend web developer , open source contributer.",
+    linkedinProfile: "https://www.linkedin.com/in/mohasin-haque-759786200/",
+  },
+  {
+    name: "Prathamesh",
+    github_username: "Prathamesh1211",
+    about:
+      "Hello, Prathamesh this side , I am a MERN stack developer and open source contributor.",
+    linkdenProfile: "https://www.linkedin.com/in/prathamesh-nikumbh-a22485246/",
+  },
+  {
+    name: "Gokul",
+    github_username: "gokulz",
+    about: "Hello I am gokul , java developer , open source contributer.",
+    linkedinProfile: "https://www.linkedin.com/in/gokul-s-7610ab1a9/",
+  },
+];
 
 export default user;
