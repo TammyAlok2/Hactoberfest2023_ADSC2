e9fb598b8211df1a4599bd8a80d0565d780c747f
Munsif Ali Misri <53260498+munsif12@users.noreply.github.com>
2023-10-07 21:35:48 +0500

    corrected the typos and added my details.

--- src/Contributer/User.js
+++ src/Contributer/User.js
@@ -1,4 +1,4 @@
-// this is contributers data
+// this is contributors data
 
 
 const user =[
@@ -27,7 +27,6 @@ const user =[
         'about':'Hello I am Mohasin , Frontend web developer , open source contributer.',
         'linkedinProfile':'https://www.linkedin.com/in/mohasin-haque-759786200/',
     },
-=======
         'name':'Prathamesh',
         'github_username': 'Prathamesh1211',
         'about':'Hello, Prathamesh this side , I am a MERN stack developer and open source contributor.',
@@ -39,6 +38,12 @@ const user =[
         'about':'Hello I am gokul , java developer , open source contributer.',
         'linkedinProfile':'https://www.linkedin.com/in/gokul-s-7610ab1a9/',
     },
+    {
+        'name':'Munsif Ali Misri',
+        'github_username': 'munsif12',
+        'about':'As a full-stack developer, I love building highly scalable products.',
+        'linkdenProfile':'https://www.linkedin.com/in/munsif-ali-misri-8191261a8/',
+    }
   
 ]
 
