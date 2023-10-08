87a7b07ef803bd27d9c44de4b701868b0877cd7a
Timir Bhingradiya <95134530+TTimir@users.noreply.github.com>
2023-10-08 17:55:30 +0530

    Create callbackfunctions.js

---
+++ callbackfunctions.js
@@ -0,0 +1,10 @@
+function createQuote(quote, callback){ 
+  var myQuote = "Like I always say, " + quote;
+  callback(myQuote); // 2
+}
+
+function logQuote(quote){
+  console.log(quote);
+}
+
+createQuote("eat your vegetables!", logQuote); // 1
